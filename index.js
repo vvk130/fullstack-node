a63ab20e253538bc6ed97674d16ee78884b869d5
vvk130 <vanessa.kasula@outlook.com>
2024-02-05 18:38:35 +0200

    backend app.get wildcard moved down in the index.js file

--- index.js
+++ index.js
@@ -24,10 +24,6 @@ if (err) {
 }
 });
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
-
 const ClothingCategory = {
     Men: "Men",
     Women: "Women",
@@ -85,7 +81,14 @@ app.get("/items/:item_id", (req, res) => {
     res.json({ item_id, q });
 });
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+
