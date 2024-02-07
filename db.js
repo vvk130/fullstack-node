2a47dc0ccc3437a80898cf8a7c151b2c2324f20a
vvk130 <vanessa.kasula@outlook.com>
2024-02-07 10:37:46 +0200

    Database pool settings added

--- db.js
+++ db.js
@@ -4,6 +4,14 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.ELEPHANTSQL_URL,
+  max: 20, 
+  idleTimeoutMillis: 30000, 
+  connectionTimeoutMillis: 2000, 
+});
+
+pool.on('error', (err, client) => {
+  console.error('Unexpected error on idle client', err);
+  process.exit(-1);
 });
 
 console.log("Database running");
