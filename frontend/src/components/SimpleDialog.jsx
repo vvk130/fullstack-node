20449147c706a922cab17f73517aabb55dc53690
vvk130 <vanessa.kasula@outlook.com>
2024-02-29 18:08:42 +0200

    Cleaning up code

--- frontend/src/components/SimpleDialog.jsx
+++ frontend/src/components/SimpleDialog.jsx
@@ -1,19 +1,16 @@
 import Dialog from "@mui/material/Dialog";
 import DialogTitle from "@mui/material/DialogTitle";
 import { useState } from "react";
-import { Button, DialogContent, DialogContentText } from "@mui/material";
+import { Button } from "@mui/material";
 import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 import shoppingCartFunctions from "../utils/shoppingCartFunctions.js";
 
 const SimpleDialog = ({ message, buttonStyle, product }) => {
   const [open, setOpen] = useState(false);
-  const myCartTotal = JSON.parse(localStorage.getItem("cartTotalValue")) || 0;
-
   const handleAddShoppingCart = () => {
     setOpen(!open);
     if (open) {
       shoppingCartFunctions.addToCart(product);
-      console.log(myCartTotal);
     }
   };
 
